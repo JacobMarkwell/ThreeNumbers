ee0a23788f2ec1d316afecb2e43f50692f43ee1c
JacobMarkwell <52938992+JacobMarkwell@users.noreply.github.com>
2024-01-29 21:19:41 -0600

    first commit

---
+++ ThreeNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+class ThreeNumbers{
+  public static void Main (string[] args) {
+    Console.Write("First number: ");
+    int numberOne = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Second number: ");
+    int numberTwo = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Third number: ");
+    int numberThree = Convert.ToInt32(Console.ReadLine());
+
+    int smallerNumber = numberOne;
+    if (numberTwo < smallerNumber){
+
+            smallerNumber = numberTwo;
+    }
+    if (numberThree < smallerNumber){
+        
+            smallerNumber = numberThree;
+  }
+    Console.WriteLine("The smaller number is" + smallerNumber);
+  
+  }
+  }
